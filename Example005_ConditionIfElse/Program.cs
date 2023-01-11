c10194603a4cfe1f14089d531889966b4572cbd1
Ruslan Shakirov <vikingsonsturm@gmail.com>
2023-01-12 00:56:47 +0300

    add Example005 COndition if-else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ruslan")
+{
+    Console.WriteLine("Asalam Aleykum, Ruslan");
+}
+else
+{
+    Console.Write("Здравствуйте, ");
+    Console.WriteLine(username);
+}
